876dbf4761c856b339133be5fafae6c2e4671ffb
kavindu praneeth <116500216+praneeth724@users.noreply.github.com>
2023-06-30 09:47:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ def open_link(link):
     webbrowser.open(link)
 
 def meeting():
-    open_link('https://us05web.zoom.us/j/86479150393?pwd=Q1FUV0pWZkNEUFU2S2ZBZ1BCN3lEQT09')
+    open_link('https://us05web.zoom.us/j/86479150393?pwd=Q1FUV0pWZkNEUFU2S2ZBZ1BCN3lEQT09')#YOUR ZOOM LINK PASTE
 
-schedule.every().friday.at("09:15").do(meeting)
+schedule.every().friday.at("09:15").do(meeting)#MEETING TIME AND DATE INSERT
 
 
 while 1:
     schedule.run_pending()
-    time.sleep(9.30)
+    time.sleep(1)
